7fac7858424812f0a5d1983ba267002986c98707
Altair Work <altairwork@mac.home>
2025-05-26 12:57:14 -0400

    add asycnioreactor dependency

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import os
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from spiders.generic_sitemap import GenericSitemapSpider
+from twisted.internet import asyncioreactor
+asyncioreactor.install()
 from twisted.internet import reactor, defer
 from scrapy import signals
 from scrapy.signalmanager import dispatcher
